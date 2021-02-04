5c4047dfac8f9708ec9c2f5aeb39205e2a71f503
Ruifeng Wang <rwang@seattleu.edu>
2021-02-04 10:20:05 -0800

    Added RollDice_Invalid_Roll_Zero_Should_Return_Zero to RollDiceHelperUnitTests.cs

---
+++ UnitTests/Helpers/RollDiceHelperUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using Mine.Models;
+using NUnit.Framework;
+using Mine.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class RollDiceHelperUnitTests
+    {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            // Arrange 
+
+            // Act 
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
