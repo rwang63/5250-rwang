838aac2733173fabf39b35040c9d6f94e6232d56
Ruifeng Wang <rwang@seattleu.edu>
2021-02-04 10:06:05 -0800

    Added HomeMenuItemTests.cs to the project

---
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mine.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class HomeMenuItemTests
+    {
+        [Test]
+        public void HomeMenuItem_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            //
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Description", result.Description);
+            Assert.AreEqual("ID", result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1, result.Value);
+        }
+        
+        [Test]
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result.Value);
+        }
+    }
+}
