aa538930d26a35d0afd0eb17403e8bb7ad817a7c
Ruifeng Wang <rwang@seattleu.edu>
2021-02-04 09:53:15 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            //
+            Assert.Pass();
+        }
+    }
+}
